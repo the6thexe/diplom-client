9d54da9eb4760606418a2aa53a01f0168aae026d
the6thexe <5051446g@gmail.com>
2023-06-20 01:32:03 +0300

    past procc

---
+++ start.sh
@@ -0,0 +1,12 @@
+ FREE=$(free -m);
+  echo "FREE: ${FREE}"
+  FREE_FINAL=$(echo "$FREE" | grep -F Mem:  | grep -o "[0-9]*" | grep -o -m1 "^[0-9]*")
+  echo "FREE_FINAL: ${FREE_FINAL}"
+  MEM_LIMIT=$(($FREE_FINAL-400))
+  echo "MEM_LIMIT: ${MEM_LIMIT}"
+  echo " - "
+
+  export MEMORY_LIMIT="${MEM_LIMIT:=3500}";
+
+  export NODE_OPTIONS="--max-old-space-size=${MEMORY_LIMIT}"
+  echo "NODE_OPTIONS: ${NODE_OPTIONS}"
